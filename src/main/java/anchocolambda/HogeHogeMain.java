e2f305ae4b0a7237f15638509f19bef21e742c41
新田八大 <@LAPTOP-U65PSE3J>
2021-03-19 17:42:13 +0900

    initialize project.

---
+++ src/main/java/anchocolambda/HogeHogeMain.java
@@ -0,0 +1,20 @@
+package anchocolambda;
+
+import lombok.extern.slf4j.Slf4j;
+
+/**
+ * initialize用のテストクラス
+ */
+@Slf4j
+public class HogeHogeMain {
+
+    /**
+     * main
+     *
+     * @param args プログラム引数
+     */
+    public static void main(String[] args) {
+        log.info("hoge hoge main.");
+    }
+
+}
